954783ae3e89fbb72b272fa8831ba8bae6fcccc4
Andres Martinez Gotor <andres.mgotor@gmail.com>
2021-12-02 13:51:57 +0100

    same change for InlineInput

--- src/sql/ConfigEditor/InlineInput.test.tsx
+++ src/sql/ConfigEditor/InlineInput.test.tsx
@@ -5,42 +5,21 @@ import { mockDatasourceOptions } from './__mocks__/datasource';
 
 const props: InlineInputProps = {
   ...mockDatasourceOptions,
-  jsonDataPath: 'foo',
+  value: 'foo',
+  onChange: jest.fn(),
 };
 
 describe('InlineInput', () => {
-  it('should show jsonData value', () => {
-    render(<InlineInput {...props} options={{ ...props.options, jsonData: { foo: 'bar' } }} />);
+  it('should show value', () => {
+    render(<InlineInput {...props} value={'bar'} />);
     expect(screen.queryByDisplayValue('bar')).toBeInTheDocument();
   });
 
-  it('should update jsonData', () => {
+  it('should call onChange with the new value', async () => {
     const testID = 'foo-id';
-    const onOptionsChange = jest.fn();
-    render(<InlineInput {...props} data-testid={testID} onOptionsChange={onOptionsChange} />);
+    const onChange = jest.fn();
+    render(<InlineInput {...props} data-testid={testID} onChange={onChange} />);
     fireEvent.change(screen.getByTestId(testID), { target: { value: 'bar' } });
-    expect(onOptionsChange).toHaveBeenCalledWith({
-      ...props.options,
-      jsonData: {
-        ...props.options.jsonData,
-        foo: 'bar',
-      },
-    });
-  });
-
-  it('should update deep nested jsonData value', () => {
-    const onOptionsChange = jest.fn();
-    const testID = 'foo-id';
-    render(<InlineInput {...props} data-testid={testID} onOptionsChange={onOptionsChange} jsonDataPath="foo.bar" />);
-    fireEvent.change(screen.getByTestId(testID), { target: { value: 'foobar' } });
-    expect(onOptionsChange).toHaveBeenCalledWith({
-      ...props.options,
-      jsonData: {
-        ...props.options.jsonData,
-        foo: {
-          bar: 'foobar',
-        },
-      },
-    });
+    expect(onChange).toHaveBeenCalled();
   });
 });
