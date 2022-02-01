153ed286687ff46b98b89cd845748bda45cfcf39
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2022-02-01 10:24:35 +0100

    Add FillValueOptions to main index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { ConnectionConfig, ConnectionConfigProps } from './ConnectionConfig';
 export { ConfigSelect, InlineInput } from './sql/ConfigEditor';
 export { ResourceSelector, ResourceSelectorProps } from './sql/ResourceSelector';
 export { SQLQuery } from './sql/types';
-export { QueryCodeEditor, FormatSelect, FillValueSelect } from './sql/QueryEditor';
+export { QueryCodeEditor, FormatSelect, FillValueSelect, FillValueOptions } from './sql/QueryEditor';
 export * from './sql/utils';
 export * from './types';
 export * from './regions';
