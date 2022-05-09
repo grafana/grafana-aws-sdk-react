4d270aac497a4939a045a3232a04091eeb818aca
Erik Sundell <erik.sundell87@gmail.com>
2022-05-09 17:45:22 +0200

    add sigv4 component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { ConnectionConfig, ConnectionConfigProps } from './ConnectionConfig';
+export { SIGV4ConnectionConfig } from './SIGV4ConnectionConfig';
 export { ConfigSelect, InlineInput } from './sql/ConfigEditor';
 export { ResourceSelector, ResourceSelectorProps } from './sql/ResourceSelector';
 export { SQLQuery } from './sql/types';
