bba341ddcc66409ec5d9a1114205bc45430c3298
Kevin Yu <kevinwcyu@users.noreply.github.com>
2022-08-26 14:48:09 -0700

    add type to change query flow

--- src/sql/types.ts
+++ src/sql/types.ts
@@ -7,4 +7,5 @@ export interface SQLQuery extends DataQuery {
   rawSQL: string;
   format?: number;
   fillMode?: { mode: FillValueOptions; value?: number };
+  meta?: { queryFlow: 'async' | 'sync' };
 }
