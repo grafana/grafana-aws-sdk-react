8863ea4bf0abe32d3390be2930c09a90a3a652d1
Kevin Yu <kevinwcyu@users.noreply.github.com>
2022-11-03 10:21:32 -0700

    Revert "Merge pull request #21 from grafana/long-running-queries"
    This reverts commit ce0695dcf286c02b497711d1e3bc6129ad650cf7, reversing
    changes made to b67da1ea30e6480718df50f61304d1305b1eafd9.

--- rollup.config.ts
+++ rollup.config.ts
@@ -26,7 +26,7 @@ const buildCjsPackage = ({ env }) => {
         },
       },
     ],
-    external: ['react', '@grafana/data', '@grafana/ui', '@grafana/runtime', 'lodash', 'rxjs'],
+    external: ['react', '@grafana/data', '@grafana/ui', 'lodash'],
     plugins: [
       typescript({
         rollupCommonJSResolveHack: false,
