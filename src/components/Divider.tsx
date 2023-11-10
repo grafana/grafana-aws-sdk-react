0bfdee0fed56d0e4f7ec48c9af4b66323241f060
Ida Štambuk <ida.stambuk@grafana.com>
2023-11-10 16:40:13 +0100

    Add divider from grafana 10

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,9 +1,14 @@
 // copied from Azure Data Explorer plugin since there is not Divider component in G<10.1
 import React from 'react';
-import { useTheme2 } from '@grafana/ui';
+import { Divider as GrafanaDivider, useTheme2 } from '@grafana/ui';
+import { config } from '@grafana/runtime';
+import { isVersionGtOrEq } from './utils/version';
 
 export function Divider() {
   const theme = useTheme2();
+  if (isVersionGtOrEq(config.buildInfo.version, '10.1.0')) {
+    return <GrafanaDivider />;
+  }
   return (
     <div
       style={{ borderTop: `1px solid ${theme.colors.border.weak}`, margin: theme.spacing(2, 0), width: '100%' }}
